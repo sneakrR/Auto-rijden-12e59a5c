880cddf01fdc3d0757f3920b3bc1d5f961b81415
Josh <2107767@talnet.nl>
2019-11-15 10:02:10 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,12 @@
+<?php
+
+$leeftijd = readline ("hoe oud ben je?");
+
+if (15.5 > $leeftijd) {
+	echo "Helaas, je mag nog niet beginnen met rijlessen";
+} else {
+	echo "Je mag beginnen met rijlessen!";
+}
+
+
+
